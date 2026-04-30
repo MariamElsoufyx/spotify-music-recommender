326e8df9d70c5cb91f2209bee517bbf612747dd9
MariamElsoufyy <mariamelsoufyx@gmail.com>
2026-04-30 15:46:32 +0300

    updated preprocessing to seperate users_interactions_cleaned to training and testing datasets

--- src/preprocessing/load_data.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-def create_spark_session(app_name: str = "SpotifyRecommender") -> SparkSession:
-    return SparkSession.builder.appName(app_name).getOrCreate()
-
-
-def load_csv(spark: SparkSession, path: str):
-    return spark.read.csv(path, header=True, inferSchema=True)
